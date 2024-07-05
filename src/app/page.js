05a678b36d237503a08a0ca00fd8be50d3c3c86b
rahulrathore2110 <rahulg27727@gmail.com>
2024-07-05 17:02:09 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <>
+    DSN
+   <iframe src="https://drive.google.com/file/d/1tHEJ_oCXCbalOO5OS4C1BRKcfEYyacL3/preview" title="description" width={1000} height={700}></iframe>
+    
+    </>
+    
+  );
+}
